daef615bd2f162ae729a2347fa94592d657d6d1c
Danny Parsons <danny@idems.international>
2025-10-22 16:36:42 +0100

    added spells and MC evaluations

--- src/evaluate_deterministic_methods.R
+++ src/evaluate_deterministic_methods.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(tidyr)
+library(purrr)
 
 
 # Setup -------------------------------------------------------------------
@@ -16,13 +17,22 @@ zimbabwe_bc_stack <- zimbabwe_bc %>%
   dplyr::select(station, date, year, month, day, season, rain, agera5_rain, est_loci:est_qm_gamma_mk) %>%
   pivot_longer(cols = c(rain, agera5_rain, est_loci:est_qm_gamma_mk), names_to = "source", values_to = "rr")
 
+# TODO start 1 Aug instead
+# 1 Sep = 245
+s_doy_start <- 245
+
 zimbabwe_bc_stack <- zimbabwe_bc_stack %>%
   group_by(station, source) %>%
   mutate(rainday = rr > 0.85,
          lag_rainday = lag(rainday),
          month = factor(month(date), levels = c(7:12, 1:6)),
-         s_year = ifelse(month %in% 1:6, year - 1, year))
+         s_year = ifelse(month %in% 1:6, year - 1, year),
+         doy = yday_366(date),
+         s_doy = (doy - s_doy_start + 1) %% 366,
+         s_doy = ifelse(s_doy == 0, 366, s_doy)
+         )
 
+rainday_sources <- c("rain", "agera5_rain", "est_loci", "est_loci_mk")
 
 # Monthly climatology -----------------------------------------------------
 
@@ -74,22 +84,22 @@ zim_annual <- zimbabwe_bc_stack %>%
             max_rain = max(rr, na.rm = TRUE)) %>%
   ungroup()
 
-zim_spells <-  zimbabwe_bc_stack %>%
+zim_spells_annual <- zimbabwe_bc_stack %>%
   group_by(station, source, s_year) %>%
   filter(source %in% c("rain", "agera5_rain", "est_loci", "est_loci_mk")) %>%
   filter(month %in% c(10:12, 1:3)) %>%
-  mutate(dry_spell_lns = spells(!rainday))
-
-zim_spells_annual <- zim_spells %>%
-  summarise(m_dry_spell = max(dry_spell_lns, na.rm = TRUE),
-            m_dry_spell = ifelse(is.infinite(m_dry_spell), NA, m_dry_spell))
+  summarise(max_dry_spell = {
+    r <- rle(!rainday)
+    max(r$lengths[r$values], na.rm = TRUE)
+  }) %>%
+  ungroup()
 
 zim_annual <- left_join(zim_annual, zim_spells_annual, by = c("station", "source", "s_year"))
 
 zim_annual_station <- zim_annual %>% 
   filter(source == "rain") %>% 
   rename(n_rain_station = n_rain, t_rain_station = t_rain, max_rain_station = max_rain,
-         mean_rain_station = mean_rain, m_dry_spell_station = m_dry_spell) %>% 
+         mean_rain_station = mean_rain, max_dry_spell_station = max_dry_spell) %>% 
   dplyr::select(-source)
 
 zim_annual_wide <- zim_annual %>% 
@@ -102,7 +112,7 @@ zim_annual_wide <- zim_annual_wide %>%
          t_rain_diff = t_rain_station - t_rain,
          max_rain_diff = max_rain_station - max_rain,
          mean_rain_diff = mean_rain_station - mean_rain,
-         m_dry_spell_diff = m_dry_spell_station - m_dry_spell)
+         max_dry_spell_diff = max_dry_spell_station - max_dry_spell)
 
 ggplot(zim_annual, 
        aes(x = year, y = n_rain, colour = source)) +
@@ -114,8 +124,8 @@ ggplot(zim_annual,
   geom_line() +
   facet_wrap(vars(station))
 
-ggplot(zim_annual_wide %>% filter(!is.na(m_dry_spell_diff)), 
-       aes(x = source, y = m_dry_spell_diff, colour = source)) +
+ggplot(zim_annual_wide %>% filter(!is.na(max_dry_spell_diff)), 
+       aes(x = source, y = max_dry_spell_diff, colour = source)) +
   geom_boxplot() +
   scale_x_discrete(drop = TRUE) +
   facet_wrap(vars(station))
@@ -126,12 +136,12 @@ zim_annual_metrics <- zim_annual_wide %>%
             t_rain_bias = mean(t_rain_diff, na.rm = TRUE),
             max_rain_bias = mean(mean_rain_diff, na.rm = TRUE),
             mean_rain_bias = mean(mean_rain_diff, na.rm = TRUE),
-            m_dry_spell_bias = mean(m_dry_spell_diff, na.rm = TRUE),
+            max_dry_spell_bias = mean(max_dry_spell_diff, na.rm = TRUE),
             n_rain_cor = cor(n_rain, n_rain_station, use = "complete.obs"),
             t_rain_cor = cor(t_rain, t_rain_station, use = "complete.obs"),
             max_rain_cor = cor(max_rain, max_rain_station, use = "complete.obs"),
             mean_rain_cor = cor(mean_rain, mean_rain_station, use = "complete.obs"),
-            m_dry_spell_cor = cor(m_dry_spell, m_dry_spell_station))
+            max_dry_spell_cor = cor(max_dry_spell, max_dry_spell_station))
 
 
 # Spell distributions -----------------------------------------------------
@@ -140,7 +150,19 @@ dry_spells <- zimbabwe_bc_stack %>%
   group_by(station, source, s_year) %>%
   filter(source %in% c("rain", "agera5_rain", "est_loci", "est_loci_mk")) %>%
   filter(month %in% c(10:12, 1:3)) %>%
-  reframe(dry_spell_length = rle(!rainday)$lengths[rle(!rainday)$values])
+  reframe(dry_spell_length = {
+    r <- rle(!rainday)
+    r$lengths[r$values]
+    })
+
+wet_spells <- zimbabwe_bc_stack %>%
+  group_by(station, source, s_year) %>%
+  filter(source %in% c("rain", "agera5_rain", "est_loci", "est_loci_mk")) %>%
+  filter(month %in% c(10:12, 1:3)) %>%
+  reframe(wet_spell_length = {
+    r <- rle(rainday)
+    r$lengths[r$values]
+  })
 
 ggplot(dry_spells, aes(x = dry_spell_length, colour = source)) +
   stat_ecdf(size = 1) +
@@ -157,22 +179,54 @@ ggplot(dry_spells, aes(x = dry_spell_length, colour = source)) +
     panel.grid.minor = element_blank()
   )
 
-ks_results <- dry_spells %>%
-  group_by(station) %>%
-  summarise(
-    D_KS_agera5 = ks.test(
-      dry_spell_length[source == "rain"],
-      dry_spell_length[source == "agera5_rain"]
-    )$statistic,
-    D_KS_est_loci = ks.test(
-      dry_spell_length[source == "rain"],
-      dry_spell_length[source == "est_loci"]
-    )$statistic,
-    D_KS_est_loci_mk = ks.test(
-      dry_spell_length[source == "rain"],
-      dry_spell_length[source == "est_loci_mk"]
-    )$statistic
+ggplot(wet_spells, aes(x = wet_spell_length, colour = source)) +
+  stat_ecdf(size = 1) +
+  scale_x_log10() +
+  facet_wrap(vars(station)) +
+  labs(
+    x = "Wet spell length (days)",
+    y = "Empirical CDF",
+    title = "Wet spell length distribution by source and station"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "top",
+    panel.grid.minor = element_blank()
   )
+
+ks_results_dry <- dry_spells %>%
+  group_by(station) %>%
+  reframe(
+    map_dfr(c("agera5_rain", "est_loci", "est_loci_mk"), function(src) {
+      test <- ks.test(
+        dry_spell_length[source == "rain"],
+        dry_spell_length[source == src]
+      )
+      tibble(
+        source = src,
+        statistic = unname(test$statistic),
+        p_value = test$p.value
+      )
+    })
+  ) %>%
+  ungroup()
+
+ks_results_wet <- wet_spells %>%
+  group_by(station) %>%
+  reframe(
+    map_dfr(c("agera5_rain", "est_loci", "est_loci_mk"), function(src) {
+      test <- ks.test(
+        wet_spell_length[source == "rain"],
+        wet_spell_length[source == src]
+      )
+      tibble(
+        source = src,
+        statistic = unname(test$statistic),
+        p_value = test$p.value
+      )
+    })
+  ) %>%
+  ungroup()
 
 # Rain given rain evaluation ----------------------------------------------
 
@@ -196,3 +250,219 @@ zim_annual_metrics <- zim_annual_wide %>%
   group_by(station, source, lag_rainday) %>%
   summarise(mad_n_rain = mean(abs(n_rain_station - n_rain)),
             mad_t_rain = mean(abs(t_rain_station - t_rain)))
+
+
+# Markov Chain Zero Order Rainday Models ----------------------------------
+
+fit_zero_order_markov <- function(data) {
+  # Fit logistic regression: P(rainday) ~ seasonal harmonics
+  glm(rainday ~ 
+        sin(2 * pi * s_doy / 366) + cos(2 * pi * s_doy / 366) +
+        sin(4 * pi * s_doy / 366) + cos(4 * pi * s_doy / 366) +
+        sin(6 * pi * s_doy / 366) + cos(6 * pi * s_doy / 366),
+      data = data,
+      family = binomial)
+}
+
+mc_models_0 <- zimbabwe_bc_stack %>%
+  filter(source %in% rainday_sources) %>%
+  group_by(source, station) %>%
+  group_modify(~ tibble(m = list(fit_zero_order_markov(.x)))) %>%
+  ungroup()
+
+doy_df <- tibble(s_doy = 1:366)
+fitted_list <- list()
+for (i in seq_len(nrow(mc_models_0))) {
+  
+  # Extract info for this model
+  src <- mc_models_0$source[i]
+  stn <- mc_models_0$station[i]
+  mod <- mc_models_0$m[[i]]
+
+  # Predict probabilities
+  preds <- predict(mod, newdata = doy_df, type = "response")
+  
+  # Combine into a tibble
+  fitted_list[[i]] <- tibble(
+    source = src,
+    station = stn,
+    s_doy = newdata$s_doy,
+    fitted = preds
+  )
+}
+fitted_doy_df_0 <- bind_rows(fitted_list)
+
+ggplot(fitted_doy_df_0, aes(x = s_doy, y = fitted, color = source)) +
+  geom_line(size = 1) +
+  facet_wrap(~ station, ncol = 2) +
+  labs(
+    title = "Predicted Seasonal Probability of Rainday",
+    x = "Day of Year",
+    y = "Predicted Probability",
+    color = "Source"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "right",
+    panel.grid.minor = element_blank()
+  )
+
+# Markov Chain First Order Rainday Models ---------------------------------
+
+fit_first_order_markov <- function(data) {
+  # Fit logistic regression: P(rainday) ~ seasonal harmonics
+  glm(rainday ~ 
+        lag_rainday +
+        sin(2 * pi * s_doy / 366) + cos(2 * pi * s_doy / 366) +
+        sin(4 * pi * s_doy / 366) + cos(4 * pi * s_doy / 366) +
+        sin(6 * pi * s_doy / 366) + cos(6 * pi * s_doy / 366),
+      data = data,
+      family = binomial)
+}
+
+mc_models_1 <- zimbabwe_bc_stack %>%
+  filter(source %in% rainday_sources) %>%
+  group_by(source, station) %>%
+  group_modify(~ tibble(m = list(fit_first_order_markov(.x)))) %>%
+  ungroup()
+
+doy_df <- expand.grid(lag_rainday = c(TRUE, FALSE), s_doy = 1:366)
+fitted_list <- list()
+for (i in seq_len(nrow(mc_models_1))) {
+  fitted_data <- doy_df
+  preds <- predict(mc_models_1$m[[i]], newdata = fitted_data, type = "response")
+  fitted_data$fitted <- preds
+  fitted_data$source <- mc_models_1$source[i]
+  fitted_data$station <- mc_models_1$station[i]
+
+  fitted_list[[i]] <- fitted_data
+}
+fitted_doy_df_1 <- bind_rows(fitted_list)
+
+ggplot(fitted_doy_df_1, aes(x = s_doy, y = fitted, color = source, linetype = lag_rainday)) +
+  geom_line(size = 1) +
+  facet_wrap(~ station) +
+  labs(
+    title = "Probability of Rainday Given Previous Day State",
+    x = "Day of Year",
+    y = "Predicted Probability",
+    color = "Source",
+    linetype = "Previous Day"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "right",
+    panel.grid.minor = element_blank()
+  ) +
+  scale_linetype_manual(
+    values = c("TRUE" = "dotted", "FALSE" = "dashed"),
+    breaks = c("TRUE", "FALSE"),   # ensures TRUE appears first in legend
+    labels = c("Rain", "Dry")
+  )
+
+# Markov Chain Zero Order Rainfall Models ---------------------------------
+
+fit_zero_order_markov_amounts <- function(data) {
+  data_rain <- data %>% filter(rainday)
+  glm(rr ~ 
+        sin(2 * pi * s_doy / 366) + cos(2 * pi * s_doy / 366) +
+        sin(4 * pi * s_doy / 366) + cos(4 * pi * s_doy / 366),# +
+        #sin(6 * pi * s_doy / 366) + cos(6 * pi * s_doy / 366),
+      data = data_rain,
+      family = Gamma(link = "log"))
+}
+
+mc_models_0_amounts <- zimbabwe_bc_stack %>%
+  group_by(source, station) %>%
+  group_modify(~ tibble(m = list(fit_zero_order_markov_amounts(.x)))) %>%
+  ungroup()
+
+doy_df <- tibble(s_doy = 1:366)
+fitted_list <- list()
+for (i in seq_len(nrow(mc_models_0_amounts))) {
+  
+  # Extract info for this model
+  src <- mc_models_0_amounts$source[i]
+  stn <- mc_models_0_amounts$station[i]
+  mod <- mc_models_0_amounts$m[[i]]
+  
+  # Predict probabilities
+  preds <- predict(mod, newdata = doy_df, type = "response")
+  
+  # Combine into a tibble
+  fitted_list[[i]] <- tibble(
+    source = src,
+    station = stn,
+    s_doy = newdata$s_doy,
+    fitted = preds
+  )
+}
+fitted_doy_df_0_amounts <- bind_rows(fitted_list)
+
+ggplot(fitted_doy_df_0_amounts %>% filter(source %in% rainday_sources), aes(x = s_doy, y = fitted, color = source)) +
+  geom_line(size = 1) +
+  facet_wrap(~ station, ncol = 2) +
+  labs(
+    title = "Mean Rainfall Amount on Rainy Days",
+    x = "Day of Year",
+    y = "Rainfall (mm)",
+    color = "Source"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "right",
+    panel.grid.minor = element_blank()
+  )
+
+
+# Markov Chain First Order Rainfall Models --------------------------------
+
+fit_first_order_markov_amounts <- function(data) {
+  data_rain <- data %>% filter(rainday)
+  glm(rr ~ 
+        lag_rainday +
+        sin(2 * pi * s_doy / 366) + cos(2 * pi * s_doy / 366) +
+        sin(4 * pi * s_doy / 366) + cos(4 * pi * s_doy / 366),# +
+        #sin(6 * pi * s_doy / 366) + cos(6 * pi * s_doy / 366),
+      data = data_rain,
+      family = Gamma(link = "log"))
+}
+
+mc_models_1_amounts <- zimbabwe_bc_stack %>%
+  group_by(source, station) %>%
+  group_modify(~ tibble(m = list(fit_first_order_markov_amounts(.x)))) %>%
+  ungroup()
+
+doy_df <- expand.grid(lag_rainday = c(TRUE, FALSE), s_doy = 1:366)
+fitted_list <- list()
+for (i in seq_len(nrow(mc_models_1_amounts))) {
+  fitted_data <- doy_df
+  preds <- predict(mc_models_1_amounts$m[[i]], newdata = fitted_data, type = "response")
+  fitted_data$fitted <- preds
+  fitted_data$source <- mc_models_1_amounts$source[i]
+  fitted_data$station <- mc_models_1_amounts$station[i]
+  
+  fitted_list[[i]] <- fitted_data
+}
+fitted_doy_df_1_amounts <- bind_rows(fitted_list)
+
+ggplot(fitted_doy_df_1_amounts %>% filter(source %in% rainday_sources), aes(x = s_doy, y = fitted, color = source, linetype = lag_rainday)) +
+  geom_line(size = 1) +
+  facet_wrap(~ station) +
+  labs(
+    title = "Mean Rainfall Amount on Rainy Days",
+    x = "Day of Year",
+    y = "Rainfall (mm)",
+    color = "Source",
+    linetype = "Previous Day"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    legend.position = "right",
+    panel.grid.minor = element_blank()
+  ) +
+  scale_linetype_manual(
+    values = c("TRUE" = "dotted", "FALSE" = "dashed"),
+    breaks = c("TRUE", "FALSE"),   # ensures TRUE appears first in legend
+    labels = c("Rain", "Dry")
+  )
--- src/helper_funs.R
+++ src/helper_funs.R
@@ -1,12 +1,6 @@
-spells <- function(x, initial_value = NA_real_) {
-  y <- mat.or.vec(length(x), 1)
-  if(length(x) > 0) {
-    y[1] <- dplyr::if_else(x[1] == 0, 0, initial_value + 1)
-    if(length(x) > 1) {
-      for(i in 2:length(x)) {
-        y[i] <- dplyr::if_else(x[i] == 0, 0, y[i - 1] + 1)
-      }
-    }
-  }
-  return(y)
+yday_366 <- function(date) {
+  temp_doy <- lubridate::yday(date)
+  temp_leap <- lubridate::leap_year(date)
+  temp_doy[(!is.na(temp_doy)) & temp_doy > 59 & (!temp_leap)] <- 1 + temp_doy[(!is.na(temp_doy)) & temp_doy > 59 & (!temp_leap)]
+  return(temp_doy)
 }
